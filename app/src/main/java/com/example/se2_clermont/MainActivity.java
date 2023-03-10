23663031c47a6b14996f590d20452f20d9b859ca
clermona <clermontanni@gmail.com>
2023-03-10 23:50:02 +0100

    set layout

--- app/src/main/java/com/example/se2_clermont/MainActivity.java
+++ app/src/main/java/com/example/se2_clermont/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.se2_clermont;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private Button buttonCalc;
+    private EditText input;
+    private TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        input = (EditText) findViewById(R.id.input);
+        output = (TextView) findViewById(R.id.output);
+        button = (Button) findViewById(R.id.button);
+        buttonCalc = (Button) findViewById(R.id.buttonCalc);
     }
 }
