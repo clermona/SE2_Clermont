ac66fc492acefca6760b2d91084b044abde39763
clermona <clermontanni@gmail.com>
2023-03-10 23:57:34 +0100

    implemented socket connection an button event

--- app/src/main/java/com/example/se2_clermont/MainActivity.java
+++ app/src/main/java/com/example/se2_clermont/MainActivity.java
@@ -2,10 +2,21 @@ package com.example.se2_clermont;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Network;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +33,51 @@ public class MainActivity extends AppCompatActivity {
         output = (TextView) findViewById(R.id.output);
         button = (Button) findViewById(R.id.button);
         buttonCalc = (Button) findViewById(R.id.buttonCalc);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                       Network thread = new Network();
+                       Thread backgroundThread = new Thread(thread);
+                       backgroundThread.start();
+
+            }
+        });
+
+
+    }
+    class Network implements Runnable {
+
+        public BufferedReader reader;
+        public PrintWriter writer;
+        public Socket socket;
+
+        public void disconnectFromServer() throws IOException {
+            reader.close();
+            writer.close();
+            socket.close();
+        }
+        public void getDataWithString(String message) throws IOException {
+            try {  writer.println(message);
+                String response = reader.readLine();
+                output.setText(response);}
+            catch(Exception ex){
+                System.out.println("Error "+ex);
+            }
+            finally{ disconnectFromServer();}
+
+        }
+        public void run() {
+
+            try {
+                socket = new Socket( "se2-isys.aau.at", 53212);
+                writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                getDataWithString(input.getText().toString());
+            } catch (Exception ex) {
+                System.out.println("Error "+ex);
+            }
+        }
     }
 }
